dfd7e961ddcd2dcf093733c62c1de8d8475d490c
Ali Salehi <nysalehi@gmail.com>
2024-05-24 22:46:41 +0200

    Bump immortals protos version and enable ASAN in debug.

--- ports/immortals-protos/portfile.cmake
+++ ports/immortals-protos/portfile.cmake
@@ -1,15 +1,17 @@
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO Immortals-Robotics/Protos
-        REF "v${VERSION}"
-        SHA512 4bfa506f7eb7ac32c850542d6c9e44720034e740d1e06305506a9f049a757751186bc54490572f44c2af9dcba09a9560e142f37be228cf4c6c7c60636c133230
-        HEAD_REF robocup-2024
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Immortals-Robotics/Protos
+    REF "v${VERSION}"
+    SHA512 f56c74832e39f1ed4cf615cf50a74022688d3c3e3e7952d5c13af770e81ff5446d3f4de28eede4cb47adeb477f1a252be8c8fce10ff9462ed32042f8acbfefa4
+    HEAD_REF robocup-2024
 )
 
 vcpkg_cmake_configure(
-        SOURCE_PATH "${SOURCE_PATH}"
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS_DEBUG
+        -DENABLE_SANITIZERS=ON
 )
 
 vcpkg_cmake_install()
