9fb4e8f18dda55d1cb8f9962672000b444ed0edf
Ali Salehi <nysalehi@gmail.com>
2024-05-24 22:21:52 +0200

    Fix immortals protos package name.

--- ports/immortals-protos/portfile.cmake
+++ ports/immortals-protos/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO Immortals-Robotics/Protos
         REF "v${VERSION}"
-        SHA512 6afdcb8f22d00eaf34ae28ece741ff99d7f5b3656104ca9eea42476eac1eaa1003f5042ebcc5575c43b54cc646519364c4f467d9d890216aa90fc17f037bfc9d
+        SHA512 4bfa506f7eb7ac32c850542d6c9e44720034e740d1e06305506a9f049a757751186bc54490572f44c2af9dcba09a9560e142f37be228cf4c6c7c60636c133230
         HEAD_REF robocup-2024
 )
 
@@ -13,7 +13,7 @@ vcpkg_cmake_configure(
 )
 
 vcpkg_cmake_install()
-vcpkg_cmake_config_fixup(PACKAGE_NAME "protos")
+vcpkg_cmake_config_fixup()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 
