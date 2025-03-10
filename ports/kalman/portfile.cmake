572b4dbd88cb23717820d74c043da832c37a7be3
Ali Salehi <nysalehi@gmail.com>
2025-03-10 12:12:00 +0100

    Fix kalman port file path.

--- ports/kalman/portfile.cmake
+++ ports/kalman/portfile.cmake
@@ -14,6 +14,7 @@ vcpkg_configure_cmake(
 )
 
 vcpkg_install_cmake()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "kalman" CONFIG_PATH lib/)
 
-file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
 file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
